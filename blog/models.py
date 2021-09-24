4ed3772ef815c691eda0396f7e012e222a6e3059
abdulaziz <abdulaziz.yosk@gmail.com>
2021-09-24 22:40:05 +0300

    created CommentForm class in forms.py, added Comment model to models.py and added form to post-detail.html

--- blog/models.py
+++ blog/models.py
@@ -47,3 +47,8 @@ class Comment(models.Model):
     user_name = models.CharField(max_length=120)
     user_email = models.EmailField()
     entered_text = models.TextField(max_length=400)
+    post = models.ForeignKey(Post, on_delete=CASCADE,
+                             related_name='comments', null=True)
+
+    def __str__(self):
+        return f'{self.user_name} | {self.post}'
